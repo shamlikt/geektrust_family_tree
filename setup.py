1a963ef6c5f966cd7b9ff102892243506e115a3c
shamlik <tshamlikt@gmail.com>
2020-01-07 01:52:38 +0530

    Add setup file to run test

---
+++ setup.py
@@ -0,0 +1,31 @@
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+class PyTestCommand(TestCommand):
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup( name ='FamilyTree',
+       version = '0.1',
+       description='Geektrust sample poject',
+       url='Not yet',
+       author = 'Shamlik',
+       author_email='tshamlikt@gmail.com',
+       package_dir={'geektrust': 'src'},
+       packages=['geektrust'],
+       tests_require=['pytest'],
+       cmdclass={'test': PyTestCommand},
+       zip_safe=False
+       )
